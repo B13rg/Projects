19ae5e3a5c4a1fd54cd3fffaae7efe212dfb9b0a
Brendan Byers <b11484@gmail.com>
2019-08-18 14:29:01 -0700

    Added an SQlite database to track downloads
    Now it will check if an item has been downloaded before, so it won't download it multiple times.

--- Reddit Saved Downloader/Reddownload.py
+++ Reddit Saved Downloader/Reddownload.py
@@ -1,61 +1,84 @@
-import imgur_downloader
+#import imgur_downloader
 import ComVars
 import praw
 import os
 import urllib.request
 import time
+import sqlite3
 
 from praw.models.reddit.submission import Submission
 from praw.models.reddit.comment import Comment
 
-directory="./Reddit Saved Downloader/subreddits/"
+DIRECTORY="./Reddit Saved Downloader/"
+DATABASE="./reddit.sqlite"
 
+def ConnectToReddit():
+	reddit = praw.Reddit(user_agent=ComVars.user_agent,
+								client_id=ComVars.client_id,
+								client_secret=ComVars.client_secret,
+								username=ComVars.username,
+								password=ComVars.password)
 
-reddit = praw.Reddit(user_agent=ComVars.user_agent,
-                     client_id=ComVars.client_id,
-                     client_secret=ComVars.client_secret,
-                     username=ComVars.username,
-                     password=ComVars.password)
+	print(reddit.user.me())
 
-print(reddit.user.me())
+	return reddit.redditor(ComVars.username)
 
-redditor = reddit.redditor(ComVars.username)
+def ConnectToDatabase():
+	db = sqlite3.connect(DATABASE)
+	cursor = db.cursor()
+	cursor.execute("PRAGMA table_info(saved)")
+	if not cursor.fetchone():
+		# Create table
+		cursor.execute('''
+			CREATE TABLE saved(permalink TEXT PRIMARY KEY, title TEXT, url TEXT, subreddit TEXT, nsfw INTEGER)
+		''')
+		db.commit()
+	return db
 
-index=0
-subreddits = {}
-total = 0
-#index = index + items._list_index
+def CheckPostDB(db, post):
+	cursor = db.cursor()
+	cursor.execute('''SELECT COUNT(*) FROM saved WHERE permalink=?''', (post.permalink,))
+	result = cursor.fetchone()
+	print("Check result: " + str(result))
+	return result[0] != 0
 
+def AddPostDB(db, post):
+	cursor = db.cursor()
+	cursor.execuste('''
+		INSERT INTO saved(permalink, title, url, subreddit, nsfw)
+		VALUES(?, ?, ?, ?, ?)''', (post.permalink, post.title, post.url, post.subreddit, post.over_18))
+	db.commit()	
+
+dbConn=ConnectToDatabase()
+redditor = ConnectToReddit()
+print("Connected to account")
 for post in redditor.saved(limit=None):
-	if type(post) is Submission:
-		title = post.title
-		link = 'https://reddit.com' + post.permalink
-		url = post.url
-
-	elif type(post) is Comment:
-		body = post.body
+	#Skip comments for now
+	if type(post) is Comment:
 		continue
-
+	print("Checking next post")
+	# Check if post has already been downloaded
+	if CheckPostDB(dbConn, post):
+		continue
+	# Lets download it
 	sub = post.subreddit.display_name
-
-	if sub not in subreddits:
-		subreddits[sub] = []
-	subreddits[sub].append({'title': title, 'url': url, 'link': link})
-	total = total + 1
-
-print("Have "+str(total)+" items")
-
-for sub in subreddits:	
-	for item in subreddits[sub]:
-		if item['url'][-4] != "." and item['url'][-5] != ".":
-			continue 
-		try:
-			newpath = directory+sub
-			newfilename = newpath+"/"+item['title']+item['url'][-5:]
-			if not os.path.exists(newpath):
-				os.makedirs(newpath)
-			elif os.path.exists(newfilename):
-				continue
-			urllib.request.urlretrieve(item['url'], newfilename)
-		except:
+	try:
+		print("Fetching: "+post.title+ " from "+post.url)
+		newpath = DIRECTORY
+		#Move nsfw into their own directory
+		if post.over_18:
+			newpath = newpath + 'nsfw/'
+		else:
+			newpath = newpath + 'subreddits/'
+		newpath = newpath+sub
+		newfilename = newpath+"/"+post.title+post.url.split("/")[-1].split("?")[0]
+		if not os.path.exists(newpath):
+			os.makedirs(newpath)
+		elif os.path.exists(newfilename):
 			continue
+		urllib.request.urlretrieve(post.url, newfilename)
+		AddPostDB(dbConn,post)
+		print("Complete")
+	except:
+		continue
+print("Download Complete")
